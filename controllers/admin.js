1bb9d0c7484b7cfa403208762a1d4ed3cacdfa32
Amar Yassa <amaryassa@yahoo.fr>
2020-01-06 13:37:57 +0100

    Edit product 126

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,35 +1,65 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-    res.render("admin/add-product", {
-      pageTitle: "Add Product",
-      path: "/admin/add-product",
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
-    });
-  };
+  res.render("admin/edit-product", {
+    pageTitle: "add Product",
+    path: "/admin/add-product",
+    editing: false
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
 
 
-  
-  exports.postAddProduct = (req, res, next) => {
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const description = req.body.description;
-    const price = req.body.price;
-    const p = new Product(title, imageUrl, description, price);
-    p.save();
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const description = req.body.description;
+  const price = req.body.price;
+  const p = new Product(null, title, imageUrl, description, price);
+  p.save();
+  res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  console.log('postAddProduct');
+  const editMode = req.query.edit;
+  console.log('editMode', editMode);
+  if (!editMode) {
     res.redirect("/");
-  };
+  }
 
-
-  exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: 'admin/products'
-      });
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if(!product) {
+      return res.redirect('/');
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      product: product
     });
-  };
-  
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+  const updatedPrice = req.body.price;
+  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDescription, updatedPrice);
+  updatedProduct.save();
+  res.redirect("/admin/products");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "admin/products"
+    });
+  });
+};
--- models/product.js
+++ models/product.js
@@ -19,8 +19,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor( title, imageUrl, description, price) {
-    // this.id = id;
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -28,14 +28,26 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = Math.random().toString();
+   
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(chemin, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if(this.id){
+        const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(chemin, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(chemin, JSON.stringify(products), err => {
+          console.log(err);
+        });
+
+      }
     });
   }
+
 
   static fetchAll(cb) {
     getProductsFromFile(cb);
