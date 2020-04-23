d64833c483880e16619b67e804f8010c1965009f
Amar Yassa <amaryassa@yahoo.fr>
2020-04-23 15:49:46 +0200

    finished section 20 upload and download --335--

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const path = require("path");
-const CONFIG = require("./util/config.json");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -8,9 +6,10 @@ const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
-
+const multer = require("multer");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
+const CONFIG = require("./util/config.json");
 
 const MONGODB_URI = CONFIG.MONGODB_URI;
 
@@ -21,6 +20,27 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + "-" + file.originalname);
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -29,8 +49,11 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(
     session({
         secret: "my secret",
@@ -39,7 +62,6 @@ app.use(
         store: store,
     })
 );
-
 app.use(csrfProtection);
 app.use(flash());
 app.use((req, res, next) => {
